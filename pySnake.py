6575a9ff0752dc185bdb8f8a1a7a769f1d2c5d50
phil-jonesQ <phil.jones.24.4@gmail.com>
2019-07-17 21:35:36 +0100

    working vs not

--- pySnake.py
+++ pySnake.py
@@ -4,11 +4,11 @@ import random
 
 
 # Global Variable
-WindowWidth = 800
-WindowHeight = 800
+WindowWidth = 400
+WindowHeight = 400
 x_POS = WindowWidth / 2
 y_POS = WindowHeight / 2
-scale = 22
+scale = 11
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -19,6 +19,8 @@ LIGHT_GREEN = (101, 152, 101)
 GREY = (128, 128, 128)
 
 snakewindow = pygame.display.set_mode((WindowWidth, WindowHeight))
+
+
 
 
 # Use the pygame clock so we can set the frame rate of the game
@@ -32,26 +34,62 @@ def draw_grid():
             pygame.draw.rect(snakewindow, RED, rect)
 
 
+def draw_snake(x, y):
+    snake = ["1"]
+    snake_pos_x = x
+    snake_pos_y = y
+    for blocks in snake:
+        rect = pygame.Rect(snake_pos_x * (scale + 1), snake_pos_y * (scale + 1), scale, scale)
+        pygame.draw.rect(snakewindow, GREEN, rect)
+
+
+def move_snake(velocity, direction, x, y):
+    #print("DEBUG: Velocity " + str(velocity) + " Direction " + str(direction) + " X = " + str(x) + " y = " + str(y))
+    if direction == 1:
+        #print(x)
+        x = x + velocity
+        print(x)
+    if direction == 0:
+        x -= velocity
+    if direction == -1:
+        y += velocity
+    if direction == 2:
+        y -= velocity
+
+    # Boundary rules
+    if x > WindowWidth:
+        x = 0
+
+    return
+
+
 
 def main():
 
     # Variables
     loop = True
 
-    pygame.init()
+    snake_pos_x = scale
+    snake_pos_y = scale
 
+    pygame.init()
     draw_grid()
+    pygame.display.set_caption("Snake V1.00")
     while loop:
 
-        pygame.display.set_caption("Snake V1.00")
+        move_snake(5, 1, snake_pos_x, snake_pos_y)
 
+        draw_snake(snake_pos_x, snake_pos_y)
 
+        snake_pos_x += 1
+
+        #print(snake_pos_x)
 
         # Update the screen
         pygame.display.flip()
 
         # Keep frame rate at 60 - clearly not needed for this type of game
-        clock.tick(60)
+        clock.tick(10)
         # Handle user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
